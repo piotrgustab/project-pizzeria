ee65551910443575482d1d57c5f6172b057d4bef
Piotr Gustab <pgustab@aol.com>
2021-06-23 20:21:27 -0400

    process-order

--- src/js/script.js
+++ src/js/script.js
@@ -60,8 +60,10 @@
         thisProduct.data = data;
 
         thisProduct.renderInMenu();
+        thisProduct.getElements();
         thisProduct.initAccordion();
-
+        thisProduct.initOrderForm();
+        thisProduct.processOrder();
         console.log('new Product:', thisProduct);
       }
 
@@ -112,9 +114,94 @@
       }
       /* toggle active class on thisProduct.element */
       thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
+
+    });
+
+  }
+
+  initOrderForm() {
+
+    const thisProduct = this;
+
+    thisProduct.form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      thisProduct.processOrder();
+    });
+
+    for (let input of thisProduct.formInputs) {
+      input.addEventListener('change', function () {
+        thisProduct.processOrder();
+      });
+    }
+
+    thisProduct.cartButton.addEventListener('click', function (event) {
+      event.preventDefault();
+      thisProduct.processOrder();
+      thisProduct.addToCart();
     });
   }
-}
+
+  processOrder() {
+
+    const thisProduct = this;
+
+    // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+    const formData = utils.serializeFormToObject(thisProduct.form);
+    console.log('formData', formData);
+
+    // set price to default price
+    let price = thisProduct.data.price;
+
+    // for every category (param)...
+    for (let paramId in thisProduct.data.params) {
+      // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+      const param = thisProduct.data.params[paramId];
+      console.log('paramId', paramId, 'param', param);
+
+      // for every option in this category
+      for (let optionId in param.options) {
+        // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+        const option = param.options[optionId];
+        console.log('optionID', optionId, 'option', option);
+
+        const optionsIf = (formData[paramId] && formData[paramId].includes(optionId));
+
+        // checking paramId in formData and if there is an optionId
+        if (optionsIf) {
+
+          // default state of the option - !option - not default
+          if (!option.default == true) {
+            //increase price
+            price += option.price;
+          }
+        } else {
+          // if default
+          if (option.default == true) {
+            // reduce price 
+            price -= option.price;
+          }
+        }
+
+        const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
+
+        if (optionImage) {
+          if (optionsIf) {
+            optionImage.classList.add(classNames.menuProduct.imageVisible);
+          }
+          else {
+            optionImage.classList.remove(classNames.menuProduct.imageVisible);
+          }
+        }
+      }
+    }
+    thisProduct.priceSingle = price;
+
+    price *= thisProduct.amountWidget.value;
+    // update calculated price in the HTML
+
+    thisProduct.priceElem.innerHTML = price;
+  }
+
 
 
   const app = {
@@ -125,6 +212,7 @@
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
       }
+      app.init();
     },
     initData: function(){
       const thisApp = this;
@@ -141,7 +229,9 @@
       thisApp.initData ();
       thisApp.initMenu();
     },
+    
   };
+  
 }
   app.init();
   
